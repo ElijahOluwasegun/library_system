7e6573a4e5956db327624ba2f384d247d412693c
ElijahOluwasegun <125356266+ElijahOluwasegun@users.noreply.github.com>
2024-11-02 21:00:06 +0300

    Update student details code

---
+++ InsertStudents.php
@@ -0,0 +1,37 @@
+<?php
+// Step 1: Get Data from the Form [Frontend]
+if (isset($_POST['Save']))
+{
+    $StudNo = $_POST['Studentno'];
+    $FN = $_POST['Firstname'];
+    $LN = $_POST['Lastname'];
+    $E = $_POST['email'];
+    $P = $_POST['phoneno'];
+    $Cp = $_POST['programme_code'];
+    $by = $_POST['book_createdby'];
+
+    //echo $FN. " ". $LN;
+
+    // Step 2 : Connect to the database
+    include("configurations/connect.php");
+    // Step 3 : Write the SQL Command
+    $stmt = $conn->prepare("INSERT INTO staff (Studentno, Firstname, Lastname, email, phoneno, programme_code, book_createdby)
+    VALUES (:s, :f, :l, :e, :p, :u)");
+    $stmt->bindParam(':s', $StudNo);
+    $stmt->bindParam(':f', $FN);
+    $stmt->bindParam(':l', $LN);
+    $stmt->bindParam(':e', $E);
+    $stmt->bindParam(':p', $P);
+    $stmt->bindParam(':c', $Cp);
+    $stmt->bindParam(':b', $by);
+    // Step 4 : Execute the SQL Command
+    if($stmt->execute())
+    {
+        echo "Student Details Successfully Saved";
+    }
+}
+
+
+
+
+?>
---
+++ studentsinterface.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add New Students</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Serif+Oriya:wght@400..700&display=swap" rel="stylesheet">
+    <!-- Do your own css styling -->
+</head>
+<body>
+    <h1 class="header">Enter Student Details: </h1>
+    <form action="InsertStudents.php" method="post" autocomplete="on">
+        <div class="outside-input-group">
+            <div class="input-group">
+                <label class="stno" for="">Student Number:</label>
+                <input type="text" name="Studentno" required>
+            </div>
+        </div>
+            <div class="outside-input-group">
+                <div class="input-group">
+                    <label for="">First Name:</label>
+                    <input type="text" name="Firstname" required>
+                </div>
+                <div class="input-group">
+                    <label for="">Last Name:</label>
+                    <input type="text" name="Lastname" required>
+                </div>
+            </div>  
+        <div class="outside-input-group">
+            <div class="input-group">
+                <label for="">Email:</label>
+                <input type="email" name="email" required>
+            </div>
+            <div class="input-group">
+                <label for="">Phone Number: </label>
+                <input type="text" name="phoneno" required>
+            </div>
+        </div>
+        <div class="outside-input-group">
+            <div class="input-group">
+                <label for="">Programme Code:</label>
+                <input type="text" name="programme_code" required>
+            </div>
+            <div class="input-group">
+                <label for="">Book Created by: </label>
+                <input type="text" name="book_createdby" required>
+            </div>
+        </div>
+        <!-- <div class="outside-input-group">
+            <div class="input-group">
+                <label for="">Category:</label>
+                <select name="Cadre" id="" required>
+                    <option value="">==== Select ====</option>
+                    <option value="Admin">System Administrator</option>
+                    <option value="Manager">Manager</option>
+                    <option value="Waiter">Waiter</option>
+                </select>
+            </div>
+        </div> -->
+        <div class="btn">
+            <button type="submit" name="Save">Submit Student Details</button>
+        </div>
+    </form>
+</body>
+</html>
